0ea300c0326c73c1ae71a1dd805e414ba6ac06b0
Alex <NoEmailThanks@null.com>
2013-11-29 22:15:58 -0800

    Player Object
    Sends debug responses from UI buttons.

---
+++ Assets/Scripts/objectMovement.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+//Movement Functions
+function north ()
+{
+ Debug.Log("Moving North");
+}//end function
+
+
+
+function west ()
+{
+	Debug.Log("Moving West");
+}//end function
+
+
+
+function east ()
+{
+	Debug.Log("Moving East");
+}//end function
+
+
+
+function south ()
+{
+	Debug.Log("Moving South");
+}//end function
---
+++ Assets/Scripts/uiMoveButton.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+public var moveDirection : String;
+public var player01 : GameObject;
+
+function Start ()
+{
+
+player01 = gameObject.Find("player01");
+
+
+}//end start
+
+
+ //from http://answers.unity3d.com/questions/242983/change-materialstextures-on-mouse-click.html
+function  OnMouseDown()
+{
+
+//Debug.Log(moveDirection);
+
+player01.SendMessage(moveDirection);
+
+
+}//from Mouse click
+    
+function OnMouseUp ()
+ 
+{
+// Debug.Log("move Not Holding");
+
+ 
+}//from Mouse Release
+
+
+
+
