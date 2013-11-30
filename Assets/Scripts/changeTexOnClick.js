e332c64c5dc539ec9032d7dedc13360862a0bfdc
Alex <NoEmailThanks@null.com>
2013-11-29 21:21:42 -0800

    Commit 1
    simple scene that loads the main scene
    boxes that change texture when clicked by the mouse

---
+++ Assets/Scripts/changeTexOnClick.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+
+
+var matArray : Material[];
+ 
+private var index : int;
+ 
+function OnMouseUp ()
+ 
+{
+ 
+   index++;
+ 
+   index = index % matArray.Length;
+ 
+   renderer.material = matArray[index];
+ 
+}
---
+++ Assets/Scripts/loadFirstLevel.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+
+
+function Start () 
+
+//TODO : Change so this loads the level passed in through Start
+
+
+
+{
+	Application.LoadLevel ("level01");
+	//Application.LoadLevelAdditive ("level01");
+	
+}
+
